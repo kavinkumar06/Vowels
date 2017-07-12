18a995e76a08b2c894cd200bb5b80baf70c57677
kavinkumar06 <kavinm09@gmail.com>
2017-07-12 15:42:08 +0530

    Create Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,33 @@
+# Vowels
+import java.io.*;
+import java.util.*;
+public class Vowels
+{
+  public static void main(String args[])
+  {
+   int i;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter a character:");
+   char ch=sc.nextInt();
+   switch(ch)
+   {
+    case:'a';
+    case:'e';
+    case:'i';
+    case:'o';
+    case:'u';
+    case:'A';
+    case:'E';
+    case:'I';
+    case:'O';
+    case:'U':i++;
+    }
+    if(i==1)
+    System.out.println("Entered character "+ch+" is Vowel");
+    else
+    if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+    System.out.println("Entered character "+ch+" is Consonent");
+    else
+    System.out.println("Not an alphabet');
+    }
+   } 
